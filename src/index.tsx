dd43cb1300c27c9a1b41bba6cd56b95c3b850ae4
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-21 21:00:35 +0300

    feat(project): add Provider and GlobalStyles to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./App";
+import { store } from "./store";
+import { GlobalStyles } from "./ui";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <GlobalStyles />
+    <App />
+  </Provider>,
+);
