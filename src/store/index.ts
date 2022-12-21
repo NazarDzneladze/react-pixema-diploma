ff38f15531aa29f02bb496c9eb16b1f08ba5bae0
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-21 21:01:43 +0300

    feat(store): add store

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import { store } from "./store";
+import { useAppSelector, useAppDispatch } from "./hooks/hooks";
+import { toggleTheme } from "./theme/themeSlice";
+
+export { store, useAppSelector, useAppDispatch, toggleTheme };
