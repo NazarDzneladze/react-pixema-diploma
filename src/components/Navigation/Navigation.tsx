472e804bbeb6bcb1a6ceca00cbd9a49c88508974
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-24 21:12:09 +0300

    feat(components): add Navigation

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,14 @@
+import { CustomNavLink } from "components/CustomNavLink/CustomNavLink";
+import { ROUTE } from "router";
+import { StyledNavigation } from "./styles";
+
+export const Navigation = () => {
+  return (
+    <StyledNavigation>
+      <CustomNavLink to={ROUTE.HOME}>Home</CustomNavLink>
+      <CustomNavLink to={ROUTE.TRENDS}>Trends</CustomNavLink>
+      <CustomNavLink to={ROUTE.FAVORITES}>Favorites</CustomNavLink>
+      <CustomNavLink to={ROUTE.SETTINGS}>Settings</CustomNavLink>
+    </StyledNavigation>
+  );
+};
---
+++ src/components/Navigation/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledNavigation = styled.nav`
+  display: grid;
+  grid-template-rows: repeat(4, auto);
+  row-gap: 25px;
+`;
+
+export { StyledNavigation };
