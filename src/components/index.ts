f269d03823e506e9890bd75c64c5dfc9f4e27020
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-24 21:52:24 +0300

    feat(components): add index.ts for Barrel pattern

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Input } from "./Input/Input";
+import { Header } from "./Header/Header";
+import { Navigation } from "./Navigation/Navigation";
+import { CustomNavLink } from "./CustomNavLink/CustomNavLink";
+
+export { Input, Navigation, Header, CustomNavLink };
