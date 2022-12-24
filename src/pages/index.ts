90004f9fa58c275741be382e7b2783300eb04681
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-24 21:14:17 +0300

    feat(pages): add Favorites, Home, NotFound, Settings, Trends pages and index.ts for Barrel pattern

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import { HomePage } from "./HomePage";
+import { NotFoundPage } from "./NotFoundPage";
+import { SettingsPage } from "./SettingsPage";
+import { FavoritesPage } from "./FavoritesPage";
+import { TrendsPage } from "./TrendsPage";
+export { HomePage, NotFoundPage, SettingsPage, FavoritesPage, TrendsPage };
