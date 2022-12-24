7106cd51b077ccea4006982f6b5d56bdac6d8c49
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-24 20:58:51 +0300

    feat(components): add CustomNavLink

---
+++ src/components/CustomNavLink/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { NavLink, PathMatch } from "react-router-dom";
+import { Color } from "ui";
+
+const StyledNavLink = styled(NavLink)<{ $isActive: PathMatch<"*"> | null }>`
+  color: ${({ $isActive }) => ($isActive ? Color.PRIMARY : Color.SECONDARY)};
+`;
+
+export { StyledNavLink };
