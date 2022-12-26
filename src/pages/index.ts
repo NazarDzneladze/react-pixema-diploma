a845bdeaf85955dabbbe8500e92dabaea8019180
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-26 21:25:26 +0300

    refactor(pages): change the import paths for the pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,8 @@
-import { HomePage } from "./HomePage";
-import { NotFoundPage } from "./NotFoundPage";
-import { SettingsPage } from "./SettingsPage";
-import { FavoritesPage } from "./FavoritesPage";
-import { TrendsPage } from "./TrendsPage";
-export { HomePage, NotFoundPage, SettingsPage, FavoritesPage, TrendsPage };
+import { HomePage } from "./HomePage/HomePage";
+import { NotFoundPage } from "./NotFoundPage/NotFoundPage";
+import { SettingsPage } from "./SettingsPage/SettingsPage";
+import { FavoritesPage } from "./FavoritesPage/FavoritesPage";
+import { TrendsPage } from "./TrendsPage/TrendsPage";
+import { SignInPage } from "./SignInPage/SignInPage";
+
+export { HomePage, NotFoundPage, SettingsPage, FavoritesPage, TrendsPage, SignInPage };
