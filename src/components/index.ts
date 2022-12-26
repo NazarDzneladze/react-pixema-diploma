ec10cc14143a7bd6db8877dabe723fd9cb1fd2f9
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-26 21:10:04 +0300

    feat(components): add RequareAuth

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { Input } from "./Input/Input";
 import { Header } from "./Header/Header";
 import { Navigation } from "./Navigation/Navigation";
 import { CustomNavLink } from "./CustomNavLink/CustomNavLink";
+import { RequareAuth } from "./RequareAuth/RequareAuth";
 
-export { Input, Navigation, Header, CustomNavLink };
+export { Input, Navigation, Header, CustomNavLink, RequareAuth };
