c98065d7599013ab9cd5ba44f98a2d0e0818d5f5
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-26 20:53:54 +0300

    feat(assets): add burger-icon and auth-background

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 import { ReactComponent as PixemaIcon } from "./icons/pixema-logo.svg";
+import AuthBackgroundImg from "./images/auth-background.png";
+import { ReactComponent as BurgerIcon } from "./icons/burger-icon.svg";
 
-export { PixemaIcon };
+export { PixemaIcon, AuthBackgroundImg, BurgerIcon };
