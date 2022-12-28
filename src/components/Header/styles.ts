17fd621e0080e8f93566901f5a8bb9ea30bde1b5
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-28 21:10:18 +0300

    refactor(components): delete Header

--- src/components/Header/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const StyledHeader = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 150px;
-`;
-
-export { StyledHeader };
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ import { Navigation } from "./Navigation/Navigation";
 import { CustomNavLink } from "./CustomNavLink/CustomNavLink";
 import { RequareAuth } from "./RequareAuth/RequareAuth";
 import { MainContent } from "./MainContent/MainContent";
-import { Header } from "./Header/Header";
 import { SideBar } from "./SideBar/SideBar";
 
-export { Input, Navigation, CustomNavLink, RequareAuth, MainContent, Header, SideBar };
+export { Input, Navigation, CustomNavLink, RequareAuth, MainContent, SideBar };
