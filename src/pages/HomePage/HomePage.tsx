158f75fb7e178f5fdb105c8023538fdc08754710
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-30 21:31:50 +0300

    feat(pages): add useEffect with dispatch to HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import { MovieAPI } from "services";
+import { useAppDispatch, useAppSelector } from "store";
+import { fetchMovies } from "store/homePage/homePageSlice";
+
 export const HomePage = () => {
+  const dispatch = useAppDispatch();
+  const { movies } = useAppSelector((state) => state.homePage);
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
+  console.log(movies);
+
   return <div>HomePage</div>;
 };
