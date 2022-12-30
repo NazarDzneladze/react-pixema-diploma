91cb7cf1b8b14b4230da2ceaa87f6948393a4c99
NazarDzneladze <nazar.dz055@mail.ru>
2022-12-30 12:45:13 +0300

    feat(assets): add favorites, home, settings and trends icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,17 @@
 import { ReactComponent as PixemaIcon } from "./icons/pixema-logo.svg";
 import AuthBackgroundImg from "./images/auth-background.png";
 import { ReactComponent as BurgerIcon } from "./icons/burger-icon.svg";
+import { ReactComponent as HomeIcon } from "./icons/home-icon.svg";
+import { ReactComponent as TrendsIcon } from "./icons/trends-icon.svg";
+import { ReactComponent as FavoritesIcon } from "./icons/favorites-icon.svg";
+import { ReactComponent as SettingsIcon } from "./icons/settings-icon.svg";
 
-export { PixemaIcon, AuthBackgroundImg, BurgerIcon };
+export {
+  PixemaIcon,
+  AuthBackgroundImg,
+  BurgerIcon,
+  HomeIcon,
+  TrendsIcon,
+  FavoritesIcon,
+  SettingsIcon,
+};
