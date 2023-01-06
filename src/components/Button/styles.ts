c9d8c34142966de2c7f96bb80b05764a4300e61d
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-06 13:42:11 +0300

    feat(components): add some styles for Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,21 +1,27 @@
 import styled, { css } from "styled-components";
+import { Color } from "ui";
 
 interface IProps {
   $searchStyle?: boolean;
 }
 
 const StyledButton = styled.button<IProps>`
-  background: none;
+  padding: 16px 10px 16px;
+
+  background-color: ${Color.PRIMARY};
+  border: none;
+  border-radius: 10px;
+  color: ${Color.WHITE};
 
   ${({ $searchStyle }) =>
     $searchStyle &&
     css`
+      padding: 0;
       position: absolute;
       top: 20px;
       right: 19px;
 
       background: none;
-      border: none;
     `}
 `;
 
