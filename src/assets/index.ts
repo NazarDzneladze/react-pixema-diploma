79988810bbbc8c381c67b0eec4adf18ba038a0e9
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-08 15:59:03 +0300

    refactor(assets): rename icons names if icons folder

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,11 +1,11 @@
 import { ReactComponent as PixemaIcon } from "./icons/pixema-logo.svg";
 import AuthBackgroundImg from "./images/auth-background.png";
-import { ReactComponent as BurgerIcon } from "./icons/burger-icon.svg";
-import { ReactComponent as HomeIcon } from "./icons/home-icon.svg";
-import { ReactComponent as TrendsIcon } from "./icons/trends-icon.svg";
-import { ReactComponent as FavoritesIcon } from "./icons/favorites-icon.svg";
-import { ReactComponent as SettingsIcon } from "./icons/settings-icon.svg";
-import { ReactComponent as SearchIcon } from "./icons/search-settings-icon.svg";
+import { ReactComponent as BurgerIcon } from "./icons/burger.svg";
+import { ReactComponent as HomeIcon } from "./icons/home.svg";
+import { ReactComponent as TrendsIcon } from "./icons/trends.svg";
+import { ReactComponent as FavoritesIcon } from "./icons/favorites.svg";
+import { ReactComponent as SettingsIcon } from "./icons/settings.svg";
+import { ReactComponent as SearchIcon } from "./icons/search-settings.svg";
 
 export {
   PixemaIcon,
