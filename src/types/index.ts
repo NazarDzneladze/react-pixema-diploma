0f20a8185c45ff919d4d9a10cf53638a66ea7e76
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-08 21:36:36 +0300

    feat(types): add IMovieResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,7 @@ import { IMovie } from "./types";
 import { IMovieAPI } from "./types";
 import { IMovieDetailsAPI } from "./types";
 import { IMovieDetails } from "./types";
+import { IUserInfo } from "./types";
+import { IMovieResponse } from "./types";
 
-export type { IMovie, IMovieAPI, IMovieDetails, IMovieDetailsAPI };
+export type { IMovie, IMovieAPI, IMovieDetails, IMovieDetailsAPI, IUserInfo, IMovieResponse };
--- src/types/types.ts
+++ src/types/types.ts
@@ -6,6 +6,12 @@ export interface IMovieAPI {
   Poster: string;
 }
 
+export interface IMovieResponse {
+  Search: IMovieAPI[];
+  totalResults: string;
+  Response: string;
+}
+
 export interface IMovie {
   title: string;
   year: string;
@@ -15,35 +21,42 @@ export interface IMovie {
 }
 
 export interface IMovieDetailsAPI {
-  Title?: string;
-  Year?: string;
-  Released?: string;
-  Runtime?: string;
-  Genre?: string;
-  Director?: string;
-  Writer?: string;
-  Actors?: string;
-  Plot?: string;
-  BoxOffice?: string;
-  Country?: string;
-  imdbRating?: string;
-  Poster?: string;
-  imdbID?: string;
+  Title: string;
+  Year: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  BoxOffice: string;
+  Country: string;
+  imdbRating: string;
+  Poster: string;
+  imdbID: string;
 }
 
 export interface IMovieDetails {
-  title?: string;
-  year?: string;
-  released?: string;
-  runtime?: string;
-  genre?: string;
-  director?: string;
-  writer?: string;
-  actors?: string;
-  plot?: string;
-  boxOffice?: string;
-  country?: string;
-  imdbRating?: string;
-  poster?: string;
-  imdbID?: string;
+  title: string;
+  year: string;
+  released: string;
+  runtime: string;
+  genre: string;
+  director: string;
+  writer: string;
+  actors: string;
+  plot: string;
+  boxOffice: string;
+  country: string;
+  imdbRating: string;
+  poster: string;
+  imdbID: string;
+}
+
+export interface IUserInfo {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }
