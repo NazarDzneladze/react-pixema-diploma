94ec9cb23bc218db69cc583c644123e270926830
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-08 21:29:48 +0300

    refactor(src): delete App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { App } from "App";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "store";
 import { GlobalStyles } from "ui";
+import { RouterProvider } from "react-router-dom";
+import { router } from "router";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,6 +12,6 @@ const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <GlobalStyles />
-    <App />
+    <RouterProvider router={router} />
   </Provider>,
 );
