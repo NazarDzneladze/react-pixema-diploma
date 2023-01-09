1cab68c8144d2451acbf0be44555d9af91a04eec
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-09 21:38:05 +0300

    feat(pages): add some Styled Components to DetailsPage

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1,25 +1,36 @@
-import { transformMovieDetails } from "mappers";
+import { IMDbIcon } from "assets";
+import { Header } from "components";
 import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { transformMovieDetails } from "services";
 import { useAppDispatch, useAppSelector } from "store";
 import { fetchMovieDetails } from "store";
-import { MoviePoster, StyledDetailsPage } from "./styles";
+import {
+  DetailsContainer,
+  DetailsHeader,
+  GenreItem,
+  GenreList,
+  MoviePoster,
+  MovieTitle,
+  HeaderInfo,
+  Runtime,
+  StyledDetailsPage,
+  Raiting,
+  DetailsPreview,
+  MovieInfo,
+  Plot,
+  OtherDetails,
+  Info,
+} from "./styles";
 
 export const DetailsPage = () => {
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { movie } = useAppSelector((state) => state.detailsPage);
-  const { imdb } = useParams();
+  const { movie } = useAppSelector((state) => state.details);
+  const { imdb = "" } = useParams();
 
   useEffect(() => {
-    if (imdb) {
-      dispatch(fetchMovieDetails(imdb));
-    }
+    dispatch(fetchMovieDetails(imdb));
   }, [dispatch, imdb]);
-
-  const handleBack = () => {
-    navigate(-1);
-  };
 
   const {
     title,
@@ -40,10 +51,46 @@ export const DetailsPage = () => {
 
   return (
     <StyledDetailsPage>
-      <MoviePoster src={poster} />
-      {genre?.split(", ").map((genre) => (
-        <li key={genre}>{genre}</li>
-      ))}
+      <Header />
+      <DetailsContainer>
+        <DetailsHeader>
+          <GenreList>
+            {genre?.split(", ").map((genre) => (
+              <GenreItem key={genre}>{genre}</GenreItem>
+            ))}
+          </GenreList>
+          <MovieTitle>{title}</MovieTitle>
+          <HeaderInfo>
+            <Raiting>
+              <IMDbIcon />
+              {imdbRating}
+            </Raiting>
+            <Runtime>{runtime}</Runtime>
+          </HeaderInfo>
+        </DetailsHeader>
+        <DetailsPreview>
+          <MoviePoster src={poster} />
+        </DetailsPreview>
+        <MovieInfo>
+          <Plot>{plot}</Plot>
+          <OtherDetails>
+            <Info>Year</Info>
+            <Info>{year}</Info>
+            <Info>Released</Info>
+            <Info>{released}</Info>
+            <Info>BoxOffice</Info>
+            <Info>{boxOffice}</Info>
+            <Info>Country</Info>
+            <Info>{country}</Info>
+            <Info>Actors</Info>
+            <Info>{actors}</Info>
+            <Info>Director</Info>
+            <Info>{director}</Info>
+            <Info>Writers</Info>
+            <Info>{writer}</Info>
+          </OtherDetails>
+        </MovieInfo>
+      </DetailsContainer>
     </StyledDetailsPage>
   );
 };
--- src/pages/DetailsPage/styles.ts
+++ src/pages/DetailsPage/styles.ts
@@ -1,8 +1,89 @@
 import styled from "styled-components";
+import { Color } from "ui";
 
-const StyledDetailsPage = styled.div``;
+const StyledDetailsPage = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr;
+`;
+
+const DetailsContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+  row-gap: 32px;
+`;
+
+const DetailsHeader = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+`;
+
+const GenreList = styled.ul`
+  display: flex;
+`;
+
+const GenreItem = styled.li`
+  color: ${Color.LIGHT};
+`;
+
+const HeaderInfo = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  column-gap: 20px;
+`;
+
+const Raiting = styled.span`
+  background-color: ${Color.GRAPHITE};
+
+  svg path {
+    fill: ${Color.WHITE_THEME};
+  }
+`;
+
+const MovieInfo = styled.div`
+  display: grid;
+  grid-template-rows: auto auto auto;
+  row-gap: 40px;
+`;
+
+const Runtime = styled.span``;
+
+const MovieTitle = styled.h3``;
+
+const DetailsPreview = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+`;
+
+const OtherDetails = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: repeat(7, auto);
+  justify-content: space-between;
+  row-gap: 20px;
+  column-gap: 60px;
+`;
+
+const Info = styled.p``;
+
+const Plot = styled.p``;
 
 const MoviePoster = styled.img``;
 
 
-export { StyledDetailsPage, MoviePoster };
+export {
+  StyledDetailsPage,
+  MoviePoster,
+  DetailsContainer,
+  DetailsHeader,
+  GenreList,
+  GenreItem,
+  MovieTitle,
+  HeaderInfo,
+  Runtime,
+  Raiting,
+  DetailsPreview,
+  MovieInfo,
+  Plot,
+  OtherDetails,
+  Info,
+};
