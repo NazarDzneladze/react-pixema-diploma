e144aa16afcb18b502784c3561406974d3c54dcf
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-12 22:21:13 +0300

    feat(assets): add next-arrow.svg

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,6 +7,7 @@ import { ReactComponent as FavoritesIcon } from "./icons/favorites.svg";
 import { ReactComponent as SettingsIcon } from "./icons/settings.svg";
 import { ReactComponent as SearchIcon } from "./icons/search-settings.svg";
 import { ReactComponent as IMDbIcon } from "./icons/imdb.svg";
+import { ReactComponent as NextSlideIcon } from "./icons/next-arrow.svg";
 
 export {
   PixemaIcon,
@@ -18,4 +19,5 @@ export {
   SettingsIcon,
   SearchIcon,
   IMDbIcon,
+  NextSlideIcon,
 };
