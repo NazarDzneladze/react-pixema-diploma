c9fcc7df4403b20f193f96b9785360d2afd18a96
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-12 22:33:03 +0300

    feat(components): add type prop for Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,16 @@
 import { StyledButton } from "./styles";
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
 interface IProps {
   children: ReactNode;
   $searchStyle?: boolean;
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
   onClick?: () => {};
 }
 
-export const Button = ({ children, $searchStyle, onClick }: IProps) => {
+export const Button = ({ children, $searchStyle, onClick, type = "submit" }: IProps) => {
   return (
-    <StyledButton $searchStyle={$searchStyle} onClick={onClick}>
+    <StyledButton $searchStyle={$searchStyle} onClick={onClick} type={type}>
       {children}
     </StyledButton>
   );
