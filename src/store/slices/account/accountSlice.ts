fb2b63593773ab8649351a6bfee0e96596dec421
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-22 20:45:44 +0300

    feat(store): add signUp in accountSlice

--- src/store/slices/account/accountSlice.ts
+++ src/store/slices/account/accountSlice.ts
@@ -1,28 +1,53 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { app } from "firebase";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { IUserInfo } from "types";
 
 interface IAccountState {
   user: IUserInfo;
+  isLoading: boolean;
 }
 
-// export const signIn = createAsyncThunk(
-//   "accountSlice/signIn",
-//   async (email: string, password: string) => {
-//     const auth = getAuth(app);
-//     const newUser = await createUserWithEmailAndPassword(auth, email, password);
-//   },
-// );
+export const signUp = createAsyncThunk<
+  IUserInfo,
+  { email: string; password: string; name: string },
+  { rejectValue: string }
+>("account/signUp", async ({ email, password, name }, { rejectWithValue }) => {
+  const auth = getAuth();
+  const { user } = await createUserWithEmailAndPassword(auth, email, password);
+  console.log(user);
+
+  return {
+    name: name,
+    email: email,
+    password: password,
+    id: user.uid,
+  };
+});
 
 const initialState: IAccountState = {
   user: {} as IUserInfo,
+  isLoading: false,
 };
 
 const accountSlice = createSlice({
-  name: "accountSlice",
+  name: "account",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(signUp.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(signUp.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+    });
+    // builder.addCase(signUp.rejected, (state, action) => {
+    //   if (action.payload) {
+    //     state.isLoading = false;
+    //     state.error = action.payload;
+    //   }
+    // });
+  },
 });
 
 export default accountSlice.reducer;
