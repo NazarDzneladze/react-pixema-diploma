a0b24e856e413fd4d19ad26c30c5ea3815a23220
NazarDzneladze <nazar.dz055@mail.ru>
2023-01-26 22:29:36 +0300

    feat(pages): add some layout for TrendsPage

--- src/pages/TrendsPage/TrendsPage.tsx
+++ src/pages/TrendsPage/TrendsPage.tsx
@@ -1,3 +1,37 @@
+import { Header, MovieList, Spinner } from "components";
+import { useEffect, useState } from "react";
+import { fetchTrends, useAppDispatch, useAppSelector } from "store";
+import { selectTrends } from "store/selectors/trendsSelector";
+import { IMovie } from "types";
+import { getRandomMovieKeyWord } from "utils";
+import { ErrorText, StyledTrendsPage } from "./styles";
+
+interface IParams {
+  search: string;
+  movies: IMovie[];
+  page: number;
+  type: string;
+}
+
 export const TrendsPage = () => {
-  return <div>TrendsPage</div>;
+  const { movies, isLoading, error } = useAppSelector(selectTrends);
+  const dispatch = useAppDispatch();
+  const [params, setParams] = useState<IParams>({
+    search: "" || getRandomMovieKeyWord(),
+    movies: movies,
+    page: 1,
+    type: "movie",
+  });
+
+  useEffect(() => {
+    dispatch(fetchTrends(params));
+  }, [dispatch, params]);
+  return (
+    <StyledTrendsPage>
+      <Header />
+      {isLoading && <Spinner />}
+      {error && <ErrorText>{error}</ErrorText>}
+      {movies && movies.length > 0 && <MovieList movies={movies} isTrends />}
+    </StyledTrendsPage>
+  );
 };
---
+++ src/pages/TrendsPage/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { Breakpoint } from "ui";
+
+const StyledTrendsPage = styled.div`
+  display: grid;
+  grid-template-rows: 56px 1fr;
+
+  ${Breakpoint.XXS} {
+    grid-template-rows: auto 1fr;
+  }
+
+  ${Breakpoint.MD} {
+    grid-template-rows: 56px 1fr;
+  }
+`;
+
+const ErrorText = styled.span`
+  place-self: center;
+
+  font-size: 54px;
+
+  color: red;
+`;
+
+export { StyledTrendsPage, ErrorText };
